e16b66ff00ffa0b31d71d44bec20663512682b0c
Azhar <azharhossain6520@gmail.com>
2023-10-13 13:29:40 +0530

    Basic Counter project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter]=useState(15)
+
+  // let counter=10
+
+  const addValue=()=>{
+    // let value=Math.floor(Math.random()*10)
+    // console.log(value)
+    // counter+=1
+    if(counter<20){
+
+      setCounter(counter+1)
+    }
+  }
+  const decreaseValue=()=>{
+    if(counter!=0){
+      setCounter(counter-1)
+    }
+  }
+  return (
+    <>
+      <h1>React</h1>
+      <h2>Counter value :{counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add Value to {counter}</button><br></br>
+      <button
+      onClick={decreaseValue}
+      >decrease Value to {counter}</button>
+    </>
+  )
+}
+
+export default App
