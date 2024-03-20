906604c2b5f8d3348217121aff8a57aae88e4a75
Azhar <azharhossain6520@gmail.com>
2024-03-20 19:46:10 +0530

    update routing

--- 07reactrouter/src/components/GitHub/GitHub.jsx
+++ 07reactrouter/src/components/GitHub/GitHub.jsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 function GitHub() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.github.com/users/SekhAzharHossain")
-      .then((Response) => Response.json())
-      .then((data) => {
-        setData(data);
-      }, []);
-  });
+  const data = useLoaderData;
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/SekhAzharHossain")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setData(data);
+  //     }, []);
+  // });
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
-      GitHub followers
+      {/* GitHub followers: {data.followers} */}
+      GitHub Name:{data.name}
+      {/* GitHub url:{data.url} */}
+      <img src={data.avatar_url} className=""></img>
     </div>
   );
 }
 
 export default GitHub;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/SekhAzharHossain");
+  return response.json();
+};
--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -12,7 +12,8 @@ import Layout from "./components/Layout.jsx";
 import About from "./components/About/About.jsx";
 import Home from "./components/Home/Home.jsx";
 import Contact from "./components/Contact/Contact.jsx";
-import GitHub from "./components/GitHub/GitHub.jsx";
+import GitHub, { githubInfoLoader } from "./components/GitHub/GitHub.jsx";
+import User from "./components/User/User.jsx";
 
 // const router = createBrowserRouter([
 //   {
@@ -41,7 +42,8 @@ const router = createBrowserRouter(
       <Route path="" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
-      <Route path="github" element={<GitHub />} />
+      <Route loader={githubInfoLoader} path="github" element={<GitHub />} />
+      <Route path="user/userid" element={<User />} />
     </Route>
   )
 );
